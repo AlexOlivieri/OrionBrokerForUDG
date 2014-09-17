ccf1c28ef89a4d7ecb736d080d3a5050bb40c990
AlexOlivieri <alex.olivieri81@gmail.com>
2014-09-17 17:07:49 +0200

    Working on the Subscription - Day 1

--- Copy of ch.hevs.softwareEngineeringUnit.IoT6.wp6.webService.OrionBrokerUDGWithUnsubscription/src/ch/hevs/softwareEngineeringUnit/IoT6/wp6/parsingManagement/ParsingPatientMovementsThread.java
+++ Copy of ch.hevs.softwareEngineeringUnit.IoT6.wp6.webService.OrionBrokerUDGWithUnsubscription/src/ch/hevs/softwareEngineeringUnit/IoT6/wp6/parsingManagement/ParsingPatientMovementsThread.java
@@ -6,19 +6,23 @@ import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import ch.hevs.softwareEngineeringUnit.IoT6.wp6.dataStructures.RoomsModeRegister;
-import ch.hevs.softwareEngineeringUnit.IoT6.wp6.dataStructures.SubscriptionRegister;
+import ch.hevs.se.IoT6.T6_5.OrionInterface.Boundary.SubscriberEntity;
+import ch.hevs.se.IoT6.T6_5.OrionInterface.Entity.UnsubscriptionData;
+import ch.hevs.softwareEngineeringUnit.IoT6.wp6.OrionBroker.NotificationManager;
 import ch.hevs.softwareEngineeringUnit.IoT6.wp6.googleSpreadsheet.Spreadsheet;
 
 import com.google.gdata.data.spreadsheet.CellEntry;
+import com.sun.jersey.api.client.WebResource;
 
-public class ParsingPatientMovementsThread extends Thread{
+public class ParsingPatientMovementsThread implements Runnable{
 	
+	private NotificationManager notificationManager;
 	private JSONObject notification;
 	
 	private final String SubscriptionDeleted = "Deleted";
 	
-	public ParsingPatientMovementsThread(JSONObject notification){
+	public ParsingPatientMovementsThread(NotificationManager notificationManager, JSONObject notification){
+		this.notificationManager = notificationManager;
 		this.notification = notification;
 	}
 	
@@ -29,106 +33,99 @@ public class ParsingPatientMovementsThread extends Thread{
 
 		String subscriptionId = (String) notification.get("subscriptionId");
 		
-		SubscriptionRegister subscription = SubscriptionRegister.getInstance();
+		if(notificationManager.getSubscriptionToMovementValue() == false){
+			unsubscribe();
+			
+		}else{
 		
-		JSONArray contextResponsesArray = (JSONArray) notification.get("contextResponses");
-		JSONObject contextResponsesElement = new JSONObject();
+			JSONArray contextResponsesArray = (JSONArray) notification.get("contextResponses");
+			JSONObject contextResponsesElement = new JSONObject();
+			
+			//JSONObject statusCode = new JSONObject();
+			JSONObject contextElements = new JSONObject();
+			
+			JSONArray attributesArray = new JSONArray();
+			JSONObject attributeElement = new JSONObject();
+			
+			//String attributeName = "";
+			
+			String id = "";
+			String attributeName = "";
+			String attributeContextValue = "";
+			String elementType = "";
+			
+			String subscriptionName = "";
+			
 		
-		//JSONObject statusCode = new JSONObject();
-		JSONObject contextElements = new JSONObject();
-		
-		JSONArray attributesArray = new JSONArray();
-		JSONObject attributeElement = new JSONObject();
-		
-		//String attributeName = "";
-		
-		String id = "";
-		String attributeName = "";
-		String attributeContextValue = "";
-		String elementType = "";
-		
-		String subscriptionName = "";
-		
+			
+			if(contextResponsesArray != null){
+			
+				Iterator<JSONObject> contextResponsesIterator = contextResponsesArray.iterator();
+				while (contextResponsesIterator.hasNext()) {
+					
+					contextResponsesElement = contextResponsesIterator.next();
+					
+					contextElements = (JSONObject) contextResponsesElement.get("contextElement");
+					
+					id = (String) contextElements.get("id");
+					elementType = (String) contextElements.get("type");
+					
+					attributesArray = (JSONArray) contextElements.get("attributes");
+					
+					if(attributesArray != null){
+						
+						Iterator<JSONObject> attributeIterator = attributesArray.iterator();
+						
+						Date d = new Date();
+						spreadsheet.setCell(1, 1, d.toGMTString());
+						
+						int cellRow = 1;
+						
+						CellEntry lastCellOccupied = spreadsheet.findLastOccupiedCell();
+						if(lastCellOccupied != null){
+							cellRow = lastCellOccupied.getCell().getRow();
+						}
+						
+						spreadsheet.setCell(cellRow+1, 1,"Subscripion from Notification");
+						spreadsheet.setCell(cellRow+1, 4,subscriptionId);
+						
+						while (attributeIterator.hasNext()) {
+							
+							attributeElement = attributeIterator.next();
+							
+							
+							attributeName = (String) attributeElement.get("name");
+							attributeContextValue = (String) attributeElement.get("value");						
 	
-		
-		if(contextResponsesArray != null){
-		
-			Iterator<JSONObject> contextResponsesIterator = contextResponsesArray.iterator();
-			while (contextResponsesIterator.hasNext()) {
-				
-				contextResponsesElement = contextResponsesIterator.next();
-				
-				contextElements = (JSONObject) contextResponsesElement.get("contextElement");
-				
-				id = (String) contextElements.get("id");
-				elementType = (String) contextElements.get("type");
-				
-				attributesArray = (JSONArray) contextElements.get("attributes");
-				
-				if(attributesArray != null){
-					
-					Iterator<JSONObject> attributeIterator = attributesArray.iterator();
-					
-					Date d = new Date();
-					spreadsheet.setCell(1, 1, d.toGMTString());
-					
-					int cellRow = 1;
-					
-					CellEntry lastCellOccupied = spreadsheet.findLastOccupiedCell();
-					if(lastCellOccupied != null){
-						cellRow = lastCellOccupied.getCell().getRow();
-					}
-					
-					spreadsheet.setCell(cellRow+1, 1,"Subscripion from Notification");
-					spreadsheet.setCell(cellRow+1, 4,subscriptionId);
-					
-					while (attributeIterator.hasNext()) {
-						
-						attributeElement = attributeIterator.next();
-						
-						
-						attributeName = (String) attributeElement.get("name");
-						attributeContextValue = (String) attributeElement.get("value");
-						
-						subscriptionName = attributeName.concat("_").concat(elementType).concat("_").concat(id);
-						
-						String subscriptionValue = subscription.getSubscription(subscriptionName);
-						
-						if(subscriptionValue == null){
 							
-							subscription.addSubscription(subscriptionName, subscriptionId);
-							subscriptionValue = subscription.getSubscription(subscriptionName);
-							
-							spreadsheet.setCell(cellRow+3, 1,"Subscription Inserted into Hashmap");
-							spreadsheet.setCell(cellRow+4, 1,"Subscription from Hashmap");
-							spreadsheet.setCell(cellRow+4, 4,subscriptionValue);
-							spreadsheet.setCell(cellRow+5, 1,"ContextValue");
-							spreadsheet.setCell(cellRow+5, 4, attributeContextValue);
-						
-						}else if(!subscriptionValue.contains(SubscriptionDeleted)){
-							
-							RoomsModeRegister comfortModeRegister = RoomsModeRegister.getInstance();
-							String roomIdentifier = elementType.concat("_").concat(id);
-							String comfortModeValue = comfortModeRegister.getRoomComfortMode(roomIdentifier);
-							if(comfortModeValue != null){
-								spreadsheet.setCell(cellRow+3, 1, roomIdentifier);
-								spreadsheet.setCell(cellRow+3, 4, comfortModeValue);
-								spreadsheet.setCell(cellRow+4, 1,"Subscripion from Hashmap");
-								spreadsheet.setCell(cellRow+4, 4,subscriptionValue);
-
-								if(subscriptionValue.equals(subscriptionId) && comfortModeValue.equals("true")){
-								
-									spreadsheet.setCell(cellRow+4, 1,"Subscription already presents in the Hashmap");							
-									spreadsheet.setCell(cellRow+6, 1, attributeContextValue);
-									
-									//TODO: ///To insert here your code to manage LED ALARM
-									// You must use for it the variable: attributeContextValue
-								}
-							}
 						}
 					}
 				}
 			}
-		}		
+		}
+	}
+	
+	private void unsubscribe(){
+		
+		String url = "http://130.206.82.228:1026/NGSI10";
+		
+		SubscriberEntity subscriberEntity = new SubscriberEntity();
+		WebResource webResource = subscriberEntity.connectEntity(url);
+		
+		String subscriptionId = notificationManager.getSubscriptionId();
+		unsubscribe(subscriberEntity, webResource, subscriptionId);
+		
+		
+		
+	}
+	
+	private void unsubscribe(SubscriberEntity subscriberEntity, WebResource webResource, String subscriptionId){
+		
+		UnsubscriptionData unsubscriptionData = new UnsubscriptionData();
+		unsubscriptionData.setSubscriptionToBeDeleted(subscriptionId);
+		JSONObject contextPayload = unsubscriptionData.getData();
+		
+		subscriberEntity.unsubscribe(webResource, "unsubscribeContext", contextPayload);
+		
 	}
 }
